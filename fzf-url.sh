7e50e210ff2dbfb6d408a468acdaeffd32c82190
xltan <lidmuse@gmail.com>
2020-11-16 22:23:13 +0800

    Fix multiline

--- fzf-url.sh
+++ fzf-url.sh
@@ -16,8 +16,8 @@ urls=($(echo "$content" |grep -oE '(https?|ftp|file)://[-A-Za-z0-9+&@#/%?=~_|!:,
 wwws=($(echo "$content" |grep -oE 'www\.[a-zA-Z](-?[a-zA-Z0-9])+\.[a-zA-Z]{2,}(/\S+)*'                  |sed 's/^\(.*\)$/http:\/\/\1/'))
 ips=($(echo  "$content" |grep -oE '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}(:[0-9]{1,5})?(/\S+)*' |sed 's/^\(.*\)$/http:\/\/\1/'))
 
-items=$(printf '%s\n' "${urls[@]}" "${wwws[@]}" "${ips[@]}" | grep -v '^$' | sort -u | nl -w3 -s '  ')
+items=$(printf '%s\n' "${urls[@]}" "${wwws[@]}" "${ips[@]}" | grep -v '^$' | sort -u | nl -w1 -s '  ')
 
 [ -z "$items" ] && exit
 
-fzf_cmd <<< $items | awk '{print $2}'| xargs -n1 -I {} $open_cmd {} >/dev/null || true
+fzf_cmd <<< "$items" | awk '{print $2}'| xargs -n1 -I {} $open_cmd {} >/dev/null || true
